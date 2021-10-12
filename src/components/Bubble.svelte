41fda1191b5cc3221959313890de22c63886fa34
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2021-10-12 22:31:34 +0300

    feat: add Bubble

---
+++ src/components/Bubble.svelte
@@ -0,0 +1,28 @@
+<span class="bubble">
+    <slot />
+</span>
+
+<style>
+    .bubble {
+        display: inline-block;
+        color: #0779e4;
+        font-size: 0.75rem;
+        position: relative;
+        background: white;
+        border-radius: 4px;
+        padding: 4px 9px;
+    }
+
+    .bubble::after {
+        position: absolute;
+        bottom: -5px;
+        left: 50%;
+        transform: translateX(-50%);
+        content: '';
+        width: 0;
+        height: 0;
+        border-style: solid;
+        border-width: 5px 5px 0 5px;
+        border-color: white transparent transparent transparent;
+    }
+</style>
