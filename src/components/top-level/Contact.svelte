1c24536e009308f1bb4a2f887ff5c31dd6f48c63
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2021-10-25 18:31:54 +0300

    fix: contact me section styling

--- src/components/top-level/Contact.svelte
+++ src/components/top-level/Contact.svelte
@@ -10,31 +10,30 @@
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
     </p>
 
-    <a class="link" href="mailto:yulian.karapetkov@gmail.com">
-        yulian.karapetkov@gmail.com
-    </a>
+    <div>
+        <a class="link" href="mailto:yulian.karapetkov@gmail.com">
+            yulian.karapetkov@gmail.com
+        </a>
 
-    <a class="button" href="mailto:yulian.karapetkov@gmail.com">
-        Hire me
-    </a>
+        <a class="button" href="mailto:yulian.karapetkov@gmail.com">
+            Hire me
+        </a>
+    </div>
 </Section>
 
 <style>
-    /* .contact {
-        display: flex;
-        max-width: 1024px;
-        margin: 0 auto;
-        padding: 5rem;
-        background-color: var(--accent-color);
-        color: white;
-    } */
-
     h2 {
         color: white;
     }
 
     p {
         margin: 0 0 3rem;
+    }
+
+    div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     }
 
     .link {
