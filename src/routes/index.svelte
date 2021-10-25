360b19e8828cb260492d80d6b6cf216c9bf3c667
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2021-10-25 18:30:33 +0300

    feat: add Skills to index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,7 @@
     import About from '../components/top-level/About.svelte';
     import Contact from '../components/top-level/Contact.svelte';
     import Hero from '../components/top-level/Hero.svelte';
+    import Skills from '../components/top-level/Skills.svelte';
 
 </script>
 
@@ -9,6 +10,8 @@
     <Hero />
 
     <About />
+
+    <Skills />
 
     <Contact />
 </div>
