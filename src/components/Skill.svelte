c70a0b8157d9dcfae0abb0d3dc4e7de0c1b787ae
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2021-10-25 18:30:10 +0300

    feat: add Skills

---
+++ src/components/Skill.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+    export let name: string;
+    export let percentage: number;
+    export let icon: string;
+
+</script>
+
+<div class="skill">
+    <span class="name">
+        {#if icon}
+            {#if icon.includes('.svg')}
+                <img class="icon" src={`/icons/${icon}`} alt={name} />
+            {:else}
+                <i class={icon} />
+            {/if}
+        {/if}
+
+        {name}
+    </span>
+
+    <div class="bar">
+        <div class="progress" style="width: {percentage}%;" />
+    </div>
+</div>
+
+<style>
+    .skill {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 1rem;
+
+        --bar-height: 0.375rem;
+    }
+
+    .name {
+        text-transform: uppercase;
+        font-size: 0.875rem;
+        text-align: center;
+        margin-bottom: 0.25rem;
+    }
+
+    i {
+        color: var(--accent-color);
+    }
+
+    .icon {
+        height: 0.875rem;
+        position: relative;
+        top: 2px;
+        filter: invert(66%) sepia(27%) saturate(6456%) hue-rotate(3deg) brightness(107%) contrast(105%);
+    }
+
+    .icon,
+    i {
+        margin-right: 0.125rem;
+    }
+
+    .bar {
+        border: 1px solid var(--accent-color);
+        border-radius: var(--border-radius);
+        height: var(--bar-height);
+        padding: 2px;
+    }
+
+    .progress {
+        background-color: var(--accent-color);
+        height: var(--bar-height);
+
+        border-radius: var(--border-radius);
+    }
+</style>
---
+++ src/components/top-level/Skills.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+    import Section from '../Section.svelte';
+    import Skill from '../Skill.svelte';
+
+    const skills = [
+        {
+            name: 'React',
+            percentage: 80,
+            icon: 'fab fa-react'
+        },
+        {
+            name: 'React Native',
+            percentage: 80,
+            icon: 'fab fa-react'
+        },
+        {
+            name: 'Svelte/Sapper',
+            percentage: 100,
+            icon: 'svelte.svg'
+        },
+        {
+            name: 'Angular',
+            percentage: 100,
+            icon: 'fab fa-angular'
+        },
+        {
+            name: 'Ionic',
+            percentage: 80,
+            icon: 'ionic.svg'
+        },
+        {
+            name: 'NestJS',
+            percentage: 60,
+            icon: 'nestjs.svg'
+        },
+        {
+            name: 'Firebase',
+            percentage: 80,
+            icon: 'firebase.svg'
+        },
+        {
+            name: 'JavaScript/TypeScript',
+            percentage: 100,
+            icon: 'fab fa-js-square'
+        },
+        {
+            name: 'HTML/CSS',
+            percentage: 100,
+            icon: 'fab fa-html5'
+        }
+    ];
+
+</script>
+
+<Section title="Skills">
+    {#each skills as skill}
+        <Skill {...skill} />
+    {/each}
+</Section>
+
+<style>
+
+</style>
