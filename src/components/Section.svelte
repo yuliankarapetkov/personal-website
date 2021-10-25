e91c907e05d1e3fff0a138f42ec0e1b1a803dbeb
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2021-10-25 18:29:59 +0300

    feat: implement Section

---
+++ src/components/Section.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    export let title: string = '';
+    export let style: 'default' | 'accent' = 'default';
+
+</script>
+
+<section class={style}>
+    <div class="content">
+        <h2>
+            {title}
+        </h2>
+
+        <slot />
+    </div>
+</section>
+
+<style>
+    section {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+        width: 100%;
+        margin: 0 auto;
+        padding: 2rem 0 5rem;
+    }
+
+    .content {
+        max-width: 1024px;
+        width: 100%;
+    }
+
+    .accent {
+        background-color: var(--accent-color);
+        color: white;
+    }
+</style>
